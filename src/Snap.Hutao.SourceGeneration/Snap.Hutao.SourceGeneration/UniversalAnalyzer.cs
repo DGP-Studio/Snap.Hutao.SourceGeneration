1768d39d859f460419860c0dcc4bae84396c8339
DismissedLight <1686188646@qq.com>
2024-06-29 13:20:39 +0800

    refactor analyzer

--- src/Snap.Hutao.SourceGeneration/Snap.Hutao.SourceGeneration/UniversalAnalyzer.cs
+++ src/Snap.Hutao.SourceGeneration/Snap.Hutao.SourceGeneration/UniversalAnalyzer.cs
@@ -17,19 +17,11 @@ namespace Snap.Hutao.SourceGeneration;
 internal sealed class UniversalAnalyzer : DiagnosticAnalyzer
 {
     private static readonly DiagnosticDescriptor typeInternalDescriptor = new("SH001", "Type should be internal", "Type [{0}] should be internal", "Quality", DiagnosticSeverity.Info, true);
-    private static readonly DiagnosticDescriptor readOnlyStructRefDescriptor = new("SH002", "ReadOnly struct should be passed with ref-like key word", "ReadOnly Struct [{0}] should be passed with ref-like key word", "Quality", DiagnosticSeverity.Info, true);
     private static readonly DiagnosticDescriptor useValueTaskIfPossibleDescriptor = new("SH003", "Use ValueTask instead of Task whenever possible", "Use ValueTask instead of Task", "Quality", DiagnosticSeverity.Info, true);
     private static readonly DiagnosticDescriptor useIsNotNullPatternMatchingDescriptor = new("SH004", "Use \"is not null\" instead of \"!= null\" whenever possible", "Use \"is not null\" instead of \"!= null\"", "Quality", DiagnosticSeverity.Info, true);
     private static readonly DiagnosticDescriptor useIsNullPatternMatchingDescriptor = new("SH005", "Use \"is null\" instead of \"== null\" whenever possible", "Use \"is null\" instead of \"== null\"", "Quality", DiagnosticSeverity.Info, true);
     private static readonly DiagnosticDescriptor useIsPatternRecursiveMatchingDescriptor = new("SH006", "Use \"is { } obj\" whenever possible", "Use \"is {{ }} {0}\"", "Quality", DiagnosticSeverity.Info, true);
     private static readonly DiagnosticDescriptor useArgumentNullExceptionThrowIfNullDescriptor = new("SH007", "Use \"ArgumentNullException.ThrowIfNull()\" instead of \"!\"", "Use \"ArgumentNullException.ThrowIfNull()\"", "Quality", DiagnosticSeverity.Info, true);
-
-
-    private static readonly ImmutableHashSet<string> RefLikeKeySkipTypes = new HashSet<string>()
-    {
-        "System.Threading.CancellationToken",
-        "System.Guid"
-    }.ToImmutableHashSet();
 
     public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
     {
@@ -38,7 +30,6 @@ internal sealed class UniversalAnalyzer : DiagnosticAnalyzer
             return new DiagnosticDescriptor[]
             {
                 typeInternalDescriptor,
-                readOnlyStructRefDescriptor,
                 useValueTaskIfPossibleDescriptor,
                 useIsNotNullPatternMatchingDescriptor,
                 useIsNullPatternMatchingDescriptor,
@@ -60,9 +51,7 @@ internal sealed class UniversalAnalyzer : DiagnosticAnalyzer
     {
         SyntaxKind[] commonTypes = [SyntaxKind.ClassDeclaration, SyntaxKind.InterfaceDeclaration, SyntaxKind.StructDeclaration, SyntaxKind.EnumDeclaration,];
         context.RegisterSyntaxNodeAction(HandleTypeShouldBeInternal, commonTypes);
-        context.RegisterSyntaxNodeAction(HandleMethodParameterShouldUseRefLikeKeyword, SyntaxKind.MethodDeclaration);
         context.RegisterSyntaxNodeAction(HandleMethodReturnTypeShouldUseValueTaskInsteadOfTask, SyntaxKind.MethodDeclaration);
-        context.RegisterSyntaxNodeAction(HandleConstructorParameterShouldUseRefLikeKeyword, SyntaxKind.ConstructorDeclaration);
 
         SyntaxKind[] expressions = [SyntaxKind.EqualsExpression, SyntaxKind.NotEqualsExpression,];
         context.RegisterSyntaxNodeAction(HandleEqualsAndNotEqualsExpressionShouldUsePatternMatching, expressions);
@@ -129,96 +118,6 @@ internal sealed class UniversalAnalyzer : DiagnosticAnalyzer
         }
     }
 
-    private static void HandleMethodParameterShouldUseRefLikeKeyword(SyntaxNodeAnalysisContext context)
-    {
-        MethodDeclarationSyntax methodSyntax = (MethodDeclarationSyntax)context.Node;
-
-        // 跳过方法定义 如 接口
-        if (methodSyntax.Body == null)
-        {
-            return;
-        }
-
-        IMethodSymbol methodSymbol = context.SemanticModel.GetDeclaredSymbol(methodSyntax)!;
-
-        if (methodSymbol.ReturnType.IsOrInheritsFrom("System.Threading.Tasks.Task"))
-        {
-            return;
-        }
-
-        if (methodSymbol.ReturnType.IsOrInheritsFrom("System.Threading.Tasks.ValueTask"))
-        {
-            return;
-        }
-
-        foreach (SyntaxToken token in methodSyntax.Modifiers)
-        {
-            // 跳过异步方法，因为异步方法无法使用 ref/in/out
-            if (token.IsKind(SyntaxKind.AsyncKeyword))
-            {
-                return;
-            }
-
-            // 跳过重载方法
-            if (token.IsKind(SyntaxKind.OverrideKeyword))
-            {
-                return;
-            }
-        }
-
-        foreach (ParameterSyntax parameter in methodSyntax.ParameterList.Parameters)
-        {
-            if (context.SemanticModel.GetDeclaredSymbol(parameter) is IParameterSymbol symbol)
-            {
-                if (IsBuiltInType(symbol.Type))
-                {
-                    continue;
-                }
-
-                if (RefLikeKeySkipTypes.Contains(symbol.Type.ToDisplayString()))
-                {
-                    continue;
-                }
-
-                if (symbol.Type.IsReadOnly && symbol.RefKind == RefKind.None)
-                {
-                    Location location = parameter.GetLocation();
-                    Diagnostic diagnostic = Diagnostic.Create(readOnlyStructRefDescriptor, location, symbol.Type);
-                    context.ReportDiagnostic(diagnostic);
-                }
-            }
-        }
-    }
-
-    private static void HandleConstructorParameterShouldUseRefLikeKeyword(SyntaxNodeAnalysisContext context)
-    {
-        ConstructorDeclarationSyntax constructorSyntax = (ConstructorDeclarationSyntax)context.Node;
-
-        foreach (ParameterSyntax parameter in constructorSyntax.ParameterList.Parameters)
-        {
-            if (context.SemanticModel.GetDeclaredSymbol(parameter) is IParameterSymbol symbol)
-            {
-                if (IsBuiltInType(symbol.Type))
-                {
-                    continue;
-                }
-
-                // 跳过 CancellationToken
-                if (symbol.Type.ToDisplayString() == "System.Threading.CancellationToken")
-                {
-                    continue;
-                }
-
-                if (symbol.Type.IsReadOnly && symbol.RefKind == RefKind.None)
-                {
-                    Location location = parameter.GetLocation();
-                    Diagnostic diagnostic = Diagnostic.Create(readOnlyStructRefDescriptor, location, symbol.Type);
-                    context.ReportDiagnostic(diagnostic);
-                }
-            }
-        }
-    }
-
     public static void HandleEqualsAndNotEqualsExpressionShouldUsePatternMatching(SyntaxNodeAnalysisContext context)
     {
         BinaryExpressionSyntax syntax = (BinaryExpressionSyntax)context.Node;
@@ -273,28 +172,5 @@ internal sealed class UniversalAnalyzer : DiagnosticAnalyzer
         Location location = syntax.GetLocation();
         Diagnostic diagnostic = Diagnostic.Create(useArgumentNullExceptionThrowIfNullDescriptor, location);
         context.ReportDiagnostic(diagnostic);
-    }
-
-    private static bool IsBuiltInType(ITypeSymbol symbol)
-    {
-        return symbol.SpecialType switch
-        {
-            SpecialType.System_Boolean => true,
-            SpecialType.System_Char => true,
-            SpecialType.System_SByte => true,
-            SpecialType.System_Byte => true,
-            SpecialType.System_Int16 => true,
-            SpecialType.System_UInt16 => true,
-            SpecialType.System_Int32 => true,
-            SpecialType.System_UInt32 => true,
-            SpecialType.System_Int64 => true,
-            SpecialType.System_UInt64 => true,
-            SpecialType.System_Decimal => true,
-            SpecialType.System_Single => true,
-            SpecialType.System_Double => true,
-            SpecialType.System_IntPtr => true,
-            SpecialType.System_UIntPtr => true,
-            _ => false,
-        };
     }
 }
